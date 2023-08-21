466bbb94d9ed476d2c170901abeba0f80185697a
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-08-21 20:45:41 +0700

    add icon and viteconfig

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 import { useQuery } from '@tanstack/react-query';
 import { Redirect } from 'wouter';
+import { motion } from 'framer-motion';
 
 // interface Data {
 //     id: number;
@@ -25,35 +26,53 @@ function App({ params }: ParamsType) {
     const { toast } = useToast();
     const { data, isLoading } = useQuery({
         queryFn: async () => {
-            const { data } = await axios.get(`http://localhost:8080/${params}`);
+            const { data } = await axios.get(
+                `https://url-shortener-pied-ten.vercel.app//${params}`
+            );
 
             return data as Data;
         },
     });
 
     useEffect(() => {
+        let random = Math.floor(Math.random() * 2);
         const randomWholeNumber = Math.floor(Math.random() * 25);
         const randomWholeNumber2 = Math.floor(Math.random() * 25);
-        let sum = Number(randomWholeNumber) - Number(randomWholeNumber2);
+        let sum = 0;
+        if (random == 0) {
+            sum = Number(randomWholeNumber) - Number(randomWholeNumber2);
+            setOperation('-');
+        } else if (random == 1) {
+            sum = Number(randomWholeNumber) + Number(randomWholeNumber2);
+            setOperation('+');
+        }
+
         console.log(randomWholeNumber);
         console.log(randomWholeNumber2);
+
+        console.log(operation + 'random :' + random);
         console.log('result ' + Number(sum));
         setNum1(randomWholeNumber);
         setNum2(randomWholeNumber2);
         setResult(sum);
-        console.log(data);
     }, []);
 
     const [input, setInput] = useState('');
     const [num1, setNum1] = useState(0);
     const [num2, setNum2] = useState(0);
     const [result, setResult] = useState(0);
+    const [operation, setOperation] = useState('');
 
     if (isLoading) {
         return (
-            <div className='h-screen flex justify-center items-center flex-col px-5 text-center'>
+            <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5 }}
+                className='h-screen flex justify-center items-center flex-col px-5 text-center'
+            >
                 <h1 className='font-mulish text-6xl font-bold'>Loading..</h1>
-            </div>
+            </motion.div>
         );
     }
 
@@ -100,26 +119,49 @@ function App({ params }: ParamsType) {
             //     </div>
             // );
             // return (<Redirect to={`/redirect/${data.full_url}`}></Redirect>)
+        } else {
+            toast({
+                title: 'Incorrect Answer ',
+                description: 'Try again ! you can do it.',
+                className: 'bg-purple',
+            });
         }
-        toast({
-            title: 'Incorrect Answer ',
-            description: 'Try again ! you can do it.',
-            className: 'bg-purple',
-        });
     }
     return (
         <div className='h-screen bg-whiteBg flex flex-col items-center justify-center'>
             {/* {isLoading ? console.log('loading') : console.log('finished')} */}
-            <h1 className='text-4xl font-mulish font-bold'>Short Math </h1>
-            <h2 className='text-2xl font-space mt-10'>
+            <motion.h1
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className='text-5xl sm:text-4xl font-mulish font-bold'
+            >
+                Short Math{' '}
+            </motion.h1>
+            <motion.h2
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.55 }}
+                className='text-2xl text-center font-space mt-10'
+            >
                 URL Shortener with simple Math quiz
-            </h2>
-            <h2 className='text-xl font-space mt-10'>
+            </motion.h2>
+            <motion.h2
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className='text-xl text-center font-space mt-10'
+            >
                 Solve math problem below to continue !
-            </h2>
-            <div className='flex items-center p-0 mt-16 justify-around w-1/3'>
-                <h3 className='text-6xl font-space font-bold '>
-                    {num1} - {num2} :
+            </motion.h2>
+            <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.65 }}
+                className='flex items-center p-0 mt-16 justify-center md:1/2  lg:w-1/3'
+            >
+                <h3 className='text-5xl font-space font-bold '>
+                    {num1} {operation} {num2} :
                 </h3>
                 <input
                     onChange={(e) => {
@@ -127,10 +169,13 @@ function App({ params }: ParamsType) {
                     }}
                     type='number'
                     placeholder='0'
-                    className='h-fit text-6xl w-1/5 bg-whiteBg outline-none font-space font-bold'
+                    className='h-fit text-5xl w-1/4  bg-whiteBg outline-none font-space font-bold'
                 />
-            </div>
-            <button
+            </motion.div>
+            <motion.button
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7 }}
                 onMouseUp={checker}
                 className='rounded-md bg-purple mt-10 text-white font-space'
             >
@@ -138,7 +183,23 @@ function App({ params }: ParamsType) {
                     {' '}
                     Continue URL{' '}
                 </span>
-            </button>
+            </motion.button>
+            <motion.h4
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5 }}
+                className='text-xl font-mulish font-bold  mt-10'
+            >
+                <span className='text-black/70'>
+                    Made By :{' '}
+                    <a
+                        href='https://github.com/DewaMadeWira'
+                        className='text-black hover:underline transition-all'
+                    >
+                        Dewa
+                    </a>{' '}
+                </span>{' '}
+            </motion.h4>
         </div>
     );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -8,6 +8,7 @@ import {
     TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { useToast } from '@/components/ui/use-toast';
+import { motion } from 'framer-motion';
 
 interface Data {
     short_url: string;
@@ -19,35 +20,61 @@ function Home() {
 
     const mutation = useMutation({
         mutationFn: async () => {
-            const { data } = await axios.post(`http://localhost:8080/`, {
-                fullUrl: input,
-            });
+            const { data } = await axios.post(
+                `https://url-shortener-pied-ten.vercel.app/`,
+                {
+                    fullUrl: input,
+                }
+            );
             return data as Data;
         },
     });
     return (
-        <div className='h-screen bg-whiteBg flex flex-col items-center justify-center'>
+        <div className='h-screen bg-whiteBg flex flex-col items-center justify-center px-5'>
             {/* {isLoading ? console.log('loading') : console.log('finished')} */}
-            <h1 className='text-4xl font-mulish font-bold'>Short Math </h1>
-            <h2 className='text-2xl font-space mt-10'>
+            <motion.h1
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className='text-5xl sm:text-4xl font-mulish font-bold'
+            >
+                Short Math{' '}
+            </motion.h1>
+            <motion.h2
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.55 }}
+                className='text-2xl text-center font-space mt-10'
+            >
                 URL Shortener with simple Math quiz
-            </h2>
+            </motion.h2>
 
-            <div className='flex flex-col items-center px-2 mt-16 justify-around'>
-                <h3 className='text-6xl font-space font-bold '>
+            <div className='flex flex-col  items-center px-2 mt-16 justify-around'>
+                <motion.h3
+                    initial={{ opacity: 0, y: -100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6 }}
+                    className='text-3xl sm:text-6xl font-space font-bold text-center'
+                >
                     Add your link here
-                </h3>
-                <input
+                </motion.h3>
+                <motion.input
+                    initial={{ opacity: 0, y: -100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.65 }}
                     onChange={(e) => {
                         setInput(e.target.value);
                     }}
                     type='text'
                     placeholder='https://google.com/'
-                    className='h-fit text-4xl bg-whiteBg outline-none mt-10 font-space 
-                    font-bold w-fit'
+                    className='h-fit text-2xl bg-whiteBg outline-none mt-10 font-space 
+                    font-bold w-fit mx-auto'
                 />
 
-                <button
+                <motion.button
+                    initial={{ opacity: 0, y: -100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.67 }}
                     onClick={() => {
                         if (input == '') {
                             toast({
@@ -84,19 +111,27 @@ function Home() {
                         {' '}
                         Create URL{' '}
                     </span>
-                </button>
+                </motion.button>
 
                 {mutation.data == null ? (
                     <></>
                 ) : (
-                    <div className='text-center'>
-                        <h4 className='text-3xl font-mulish font-bold  mt-10'>
+                    <motion.div className='text-center'>
+                        <motion.h4
+                            initial={{ opacity: 0, x: -100 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className='text-3xl font-mulish font-bold  mt-10'
+                        >
                             <span className=''>Your shortened link :</span>{' '}
-                        </h4>
+                        </motion.h4>
                         <TooltipProvider>
                             <Tooltip>
                                 <TooltipTrigger>
-                                    <h4
+                                    <motion.h4
+                                        initial={{ opacity: 0, x: -100 }}
+                                        whileInView={{ opacity: 1, x: 0 }}
+                                        transition={{ duration: 0.55 }}
                                         onClick={() => {
                                             toast({
                                                 title: 'Text copied !',
@@ -111,15 +146,31 @@ function Home() {
                                         className='text-2xl font-space   mt-10 hover:cursor-pointer '
                                     >
                                         {`${window.location.href}${mutation.data.short_url}`}
-                                    </h4>
+                                    </motion.h4>
                                 </TooltipTrigger>
                                 <TooltipContent className='bg-black text-white border-none font-space mt-[100rem]'>
                                     <p>Click to copy</p>
                                 </TooltipContent>
                             </Tooltip>
                         </TooltipProvider>
-                    </div>
+                    </motion.div>
                 )}
+                <motion.h4
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='text-xl font-mulish font-bold  mt-10'
+                >
+                    <span className='text-black/70'>
+                        Made By :{' '}
+                        <a
+                            href='https://github.com/DewaMadeWira'
+                            className='text-black hover:underline transition-all'
+                        >
+                            Dewa
+                        </a>{' '}
+                    </span>{' '}
+                </motion.h4>
             </div>
         </div>
     );
